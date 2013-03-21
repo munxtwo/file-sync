7c428b51f26717ac64b6b041d54ce1eb76493d19
munxtwo <munxtwo@gmail.com>
2013-03-21 00:16:52 -0700

    Added buttons and scrollpanes to ui. Added event listeners to buttons.

---
+++ src/controller/EventListeners.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import view.Panel;
+
+/**
+ * @author ktay
+ *
+ */
+public class EventListeners {
+	
+	private Panel panel;
+	
+	public EventListeners(Panel panel) {
+		this.panel = panel;
+	}
+	
+	public String getFolderPath() {
+		String chosenDirPath = "";
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		
+		int returnVal = fileChooser.showOpenDialog(panel);
+		
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File chosenDir = fileChooser.getSelectedFile();
+			chosenDirPath = chosenDir.getAbsolutePath();
+		}
+		return chosenDirPath;
+	}
+	
+	public class BrowseSourceButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String path = getFolderPath();
+			panel.createSourcePane(path);
+			panel.revalidate();
+			panel.repaint();
+//			panel.updateUI();
+		}
+	}
+	
+	public class BrowseTargetButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String path = getFolderPath();
+			panel.createTargetPane(path);
+			panel.updateUI();
+		}
+	}
+
+}
--- src/view/Panel.java
+++ src/view/Panel.java
@@ -6,11 +6,16 @@ package view;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 
 import lib.GBC;
+import controller.EventListeners;
+import controller.EventListeners.BrowseSourceButtonListener;
+import controller.EventListeners.BrowseTargetButtonListener;
 
 /**
  * @author ktay
@@ -19,29 +24,89 @@ import lib.GBC;
 public class Panel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	public JScrollPane sourceScrollPane;
+	public JScrollPane targetScrollPane;
+	public JButton browseSourceButton;
+	public JButton browseTargetButton;
+	public JFileChooser sourceFileChooser;
+	public JFileChooser targetFileChooser;
+	
 	public Panel() {
 		setLayout(new GridBagLayout());
-		createSourceFolder();
-		createDestFolder();
+		// Buttons
+		createBrowseSourceButton();
+		createBrowseTargetButton();
+		createPanelDivider();
+	}
+	
+	public void createBrowseSourceButton() {
+		String browseSourceButtonLabel = "Choose Source Folder";
+		String browseSourceButtonToolTipText = "The source folder contains the files you would like to sync.";
+		browseSourceButton = new JButton(browseSourceButtonLabel);
+		browseSourceButton.setToolTipText(browseSourceButtonToolTipText);
+		
+		BrowseSourceButtonListener browseSourceButtonListener = new EventListeners(this).new BrowseSourceButtonListener();
+		browseSourceButton.addActionListener(browseSourceButtonListener);
+		
+		add(browseSourceButton, new GBC(0, 0, 1, 1)
+			.setAnchor(GridBagConstraints.NORTHWEST)
+			.setWeight(1, 0.05)
+			.setFill(GridBagConstraints.NONE)
+			.setInsets(5));
+	}
+	
+	public void createBrowseTargetButton() {
+		String browseTargetButtonLabel = "Choose Target Folder";
+		String browseTargetButtonToolTipText = "The target folder is the folder you would like to sync files to.";
+		browseTargetButton = new JButton(browseTargetButtonLabel);
+		browseTargetButton.setToolTipText(browseTargetButtonToolTipText);
+		
+		BrowseTargetButtonListener browseTargetButtonListener = new EventListeners(this).new BrowseTargetButtonListener();
+		browseTargetButton.addActionListener(browseTargetButtonListener);
+		
+		add(browseTargetButton, new GBC(4, 0, 1, 1)
+			.setAnchor(GridBagConstraints.NORTHWEST)
+			.setWeight(1, 0.05)
+			.setFill(GridBagConstraints.NONE)
+			.setInsets(5));
+	}
+	
+	public JFileChooser createFileChooser(String path) {
+		JFileChooser fileChooser = new JFileChooser(path);
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		fileChooser.setControlButtonsAreShown(false);
+		fileChooser.setMultiSelectionEnabled(true);
+		return fileChooser;
 	}
 
-	public void createSourceFolder() {
-		JScrollPane scrollPaneSource = new JScrollPane(new JFileChooser("C:/"));
-		add(scrollPaneSource, new GBC(0, 5, 3, 1)
-			.setAnchor(GridBagConstraints.LAST_LINE_START)
-			.setFill(GridBagConstraints.BOTH).setWeight(0.5, 0.5)
+	public void createSourcePane(String path) {
+		sourceFileChooser = createFileChooser(path);
+		sourceScrollPane = new JScrollPane(sourceFileChooser);
+		
+		add(sourceScrollPane, new GBC(0, 1, 3, 9)
+			.setAnchor(GridBagConstraints.FIRST_LINE_START)
+			.setFill(GridBagConstraints.BOTH)
+			.setWeight(1, 1)
 			.setInsets(0, 5, 5, 5));
 	}
 	
-	public void createDestFolder() {
-		JScrollPane scrollPaneDest = new JScrollPane(new JFileChooser("C:/"));
-		add(scrollPaneDest, new GBC(3, 5, 3, 1)
-			.setAnchor(GridBagConstraints.LAST_LINE_END)
-			.setFill(GridBagConstraints.BOTH).setWeight(0.5, 0.5)
+	public void createTargetPane(String path) {
+		targetFileChooser = createFileChooser(path);
+		targetScrollPane = new JScrollPane(targetFileChooser);
+		
+		add(targetScrollPane, new GBC(4, 1, 3, 9)
+			.setAnchor(GridBagConstraints.FIRST_LINE_END)
+			.setFill(GridBagConstraints.BOTH)
+			.setWeight(1, 1)
 			.setInsets(0, 5, 5, 5));
 	}
 	
-	
+	public void createPanelDivider() {
+		add(new JSeparator(JSeparator.VERTICAL), new GBC(3, 0, 1, 10)
+			.setAnchor(GridBagConstraints.CENTER)
+			.setFill(GridBagConstraints.BOTH)
+			.setInsets(0, 5, 5, 5));
+	}
 
 }

17db1ead4ac1cfc505b40c80c3aeec29e20719ec
munxtwo <munxtwo@gmail.com>
2013-03-21 00:17:36 -0700

    Added menu items to Menu.

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -3,7 +3,9 @@
  */
 package view;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * @author ktay
@@ -14,7 +16,48 @@ public class Menu extends JMenuBar {
 	private static final long serialVersionUID = 1L;
 
 	public Menu() {
-		
+		createFileMenu();
+		createEditMenu();
+		createHelpMenu();
 	}
 
+	public void createFileMenu() {
+
+		JMenu fileMenu = new JMenu("File");
+		add(fileMenu);
+
+		JMenuItem browseDir = new JMenuItem("Browse Dir ...");
+		fileMenu.add(browseDir);
+//		BrowseDirListener browseDirListener = new Actions().new BrowseDirListener();
+//		browseDir.addActionListener(browseDirListener);
+
+		fileMenu.addSeparator();
+		JMenuItem exit = new JMenuItem("Exit");
+		fileMenu.add(exit);
+
+//		ExitListener exitListener = new Actions().new ExitListener();
+//		exit.addActionListener(exitListener);
+	}
+	
+	public void createEditMenu() {
+		JMenu editMenu = new JMenu("Edit");
+		add(editMenu);
+
+		JMenuItem changeDefaultDir = new JMenuItem("Change Default Dir");
+		editMenu.add(changeDefaultDir);
+
+//		ChangeDefaultDirListener changeDefaultDirListener = new Actions().new ChangeDefaultDirListener();
+//		changeDefaultDir.addActionListener(changeDefaultDirListener);
+	}
+
+	public void createHelpMenu() {
+		JMenu helpMenu = new JMenu("Help");
+		add(helpMenu);
+
+		JMenuItem about = new JMenuItem("About FileSyncer");
+		helpMenu.add(about);
+
+//		AboutListener aboutListener = new Actions().new AboutListener();
+//		about.addActionListener(aboutListener);
+	}
 }
